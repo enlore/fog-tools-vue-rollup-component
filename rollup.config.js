641c404250ad72f2e2fa860a902205ec3bcc2eae
enlore <n.e.lorenson@gmail.com>
2017-09-06 09:35:49 -0500

    init

---
+++ rollup.config.js
@@ -0,0 +1,83 @@
+/* jshint esversion: 6, asi: true */
+import alias from 'rollup-plugin-alias'
+import vue from 'rollup-plugin-vue'
+import buble from 'rollup-plugin-buble'
+
+import nodeResolve from 'rollup-plugin-node-resolve'
+import commonjs from 'rollup-plugin-commonjs'
+import nodeGlobals from 'rollup-plugin-node-globals'
+//import replace from 'rollup-plugin-replace'
+
+import butternut from 'rollup-plugin-butternut'
+import livereload from 'rollup-plugin-livereload'
+import serve from 'rollup-plugin-serve'
+
+//import re from 'rollup-plugin-re'
+//import gitVersion from 'rollup-plugin-git-version'
+
+const plugins = [
+    alias({
+        vue$: 'vue/dist/vue.common.js'
+    }),
+    vue({
+        css: './public/assets/css/app.css'
+    }),
+    buble({
+        objectAssign: 'Object.assign'
+    }),
+    nodeResolve({
+        jsnext: true,
+        main: true,
+        browser: true
+    }),
+    //replace({
+        //include: 'minimatch or array of minimatch (all files processed if omitted)',
+        //exclude: 'same deal',
+        //patterns: [
+            //{
+            ////include: 'include & exclude available for rule scope also',
+            ////exclude: '',
+            ////match: /re goes here/,
+            ////test: 'not sure of difference in effect between this and match',
+            ////replace: 'what gets replaced in'
+            ////text: 'exports = "content"', // i don't know what this does
+            ////file: './filename.js' // replace with given relative file?
+            ////transform: (code, id) {} // return value is replacement
+            //},
+
+        //],
+        //defines: {},
+        //replaces: {
+            //$API_ENDPOINT: "api-endpoint"
+        //},
+    //}),
+    commonjs(),
+    nodeGlobals()
+]
+
+const config = {
+    entry: './src/app.js',
+    dest: './public/assets/js/app.js',
+    format: 'umd',
+    sourceMap: true,
+    plugins: plugins
+}
+
+const isProduction = process.env.NODE_ENV === `production`
+const isDevelopment = process.env.NODE_ENV === `development`
+
+if (isProduction) {
+    config.sourceMap = false
+    config.plugins.push(butternut)
+}
+
+if (isDevelopment) {
+    config.plugins.push(livereload())
+    config.plugins.push(serve({
+        contentBase: './public/',
+        port: 8080,
+        open: true
+    }))
+}
+
+export default config
---
+++ src/components/App.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="app">
+    <img src="assets/images/logo.png">
+    <h1>{{ msg }}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      msg: 'Vue.js - The Progressive JavaScript Framework'
+    }
+  }
+}
+</script>
+
+<style>
+body {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    height: 100%;
+    margin: 0;
+}
+
+.app {
+    color: #444;
+    margin-top: 100px;
+    max-width: 600px;
+    font-family: Helvetica, sans-serif;
+    text-align: center;
+    display: flex;
+    align-items: center;
+}
+
+h1 {
+  padding: 0 .25em;
+}
+</style>
